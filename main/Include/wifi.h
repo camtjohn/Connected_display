1982fcfd71dc08a2289c583340bf5d1b6a3b09ad
Cameron J <cameron.johnson@resideo.com>
2025-12-07 11:41:39 -0600

    Flashed_to_Rents_Device

--- main/Include/wifi.h
+++ main/Include/wifi.h
@@ -1,10 +1,12 @@
 #ifndef WIFI_H
 #define WIFI_H
 
-#define WIFI_SSID      "Loveshack"
-#define WIFI_PASS      "Babyloveshack"
+// #define WIFI_SSID      "Loveshack"
+// #define WIFI_PASS      "Babyloveshack"
 // #define WIFI_SSID      "jolene"
 // #define WIFI_PASS      "donttake"
+#define WIFI_SSID      "cocacola"
+#define WIFI_PASS      "worldpeace"
 #define INIT_WIFI_MAXIMUM_RETRY  5
 
 void Wifi__Start(void);
